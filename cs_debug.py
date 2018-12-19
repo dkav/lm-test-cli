025a2df064f2dba9bf43bd075095f4c4563cadaa
Darren Kavanagh <dkav@users.noreply.github.com>
2018-12-19 11:36:24 -0800

    Add description

--- cs_debug.py
+++ cs_debug.py
@@ -1,7 +1,12 @@
-#lu.call_circuitscape(cfg.CSPATH, outConfigFile)
+# Code snippet for debugging Circuitscape.
+
+# Replace following line:
+# lu.call_circuitscape(cfg.CSPATH, outConfigFile)
+
 import imp, pdb
+
 cs_run = imp.load_source(
-    "cs_run", 
+    "cs_run",
     "E:\\cs-workspace\\cs_testing\\test_scripts\\cs_run.py")
 pdb.set_trace()
 cs_run.main(out_config_file)

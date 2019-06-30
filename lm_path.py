1126b711915174002f0448917f6ae96f0f3bff29
Darren Kavanagh <dkav@users.noreply.github.com>
2019-06-30 11:34:08 -0700

    Use fork for testing

--- lm_path.py
+++ lm_path.py
@@ -5,7 +5,7 @@ import os
 import sys
 
 
-LM_PATH = '..\\..\\lm-repo\\toolbox\\scripts'
+LM_PATH = '..\\..\\lm-fork\\toolbox\\scripts'
 
 TST_PATH = (os.path.abspath(
     os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')))

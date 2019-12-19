03061b37a09f09f53738d72e3fd0e7897169182f
Darren Kavanagh <dkav@users.noreply.github.com>
2019-12-19 14:30:04 -0800

    Update cs_run path

--- cs_debug.py
+++ cs_debug.py
@@ -9,6 +9,6 @@ import imp, pdb
 
 cs_run = imp.load_source(
     "cs_run",
-    "F:\\cs-workspace\\cs_testing\\cs-test-cli\\cs_run.py")
+    "F:\\cs_testing\\cs-test-cli\\cs_run.py")
 pdb.set_trace()
 cs_run.main(outConfigFile)

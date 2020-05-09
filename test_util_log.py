cff12002096504e74d6beadc1ba48121ae925e6d
Darren Kavanagh <dkav@users.noreply.github.com>
2020-05-08 22:33:01 -0700

    Test log file upgrade without model run

--- test_util_log.py
+++ test_util_log.py
@@ -4,9 +4,11 @@ Assumes Linkage Mapper scripts and test data are in their default folders.
 """
 
 import os
+from shutil import copyfile
+from datetime import datetime
 
-import lm_path
 import lm_proj
+import lm_prj_dir
 
 import logfile_upgrade as logfile_up
 
@@ -21,8 +23,17 @@ def in_params(prj_dir):
 
 def main():
     """Run model."""
-    logfile_up.main(
-        in_params(lm_path.dir_paths(lm_proj.proj_name())[0]))
+    proj_dir, test_dir = lm_prj_dir.lm_dirs(lm_proj.proj_name())
+    log_file = datetime.now().strftime("%Y_%m_%d_%H%M_Linkage Mapper.txt")
+    tst_lfile = os.path.join(test_dir, 'lm-inputs', 'lm_v1_1_log.txt')
+    log_dir = os.path.join(proj_dir, 'run_history', 'log')
+
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    copyfile(tst_lfile, os.path.join(log_dir, log_file))
+
+    logfile_up.main(in_params(proj_dir))
 
 
 if __name__ == "__main__":

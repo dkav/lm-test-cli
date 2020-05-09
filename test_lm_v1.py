55f72a176f66fe8d9b38ef3ea9ab3f6cfa178672
Darren Kavanagh <dkav@users.noreply.github.com>
2020-05-08 22:31:05 -0700

    Update inputs to match lm t1 test

--- test_lm_v1.py
+++ test_lm_v1.py
@@ -1,6 +1,8 @@
 """Script to run Linkage Pathways Tool Version 1.
 
 Assumes Linkage Mapper scripts and test data are in their default folders.
+
+Use commit 58c4e3b rather than v1.1.0 because of intermediate cores bug.
 """
 
 import os
@@ -26,13 +28,13 @@ def in_params(prj_dir, test_dir):
          "Cost-Weighted & Euclidean",  # Network Adjacency Method (7)
          '#',  # Core Area Distances Text File (8)
          "true",  # Step 3 - Calculate CWD and LCP (9)
-         "false",  # Drop Corridors that Intersect Core Areas (10)
+         "true",  # Drop Corridors that Intersect Core Areas (10)
          "true",  # Step 4 - Prune Network (11)
          1,  # Option A - Maxium Number of Connected NN (12)
          "Euclidean",  # Option B - NN Measurement Unit (13)
          "true",  # Option C - Connect NC (14)
          "true",  # Step 5 - Calculate, Normalize and Mosaic Corridors (15)
-         "#",  # Bounding Circles Buffer Distance(16)
+         10000,  # Bounding Circles Buffer Distance(16)
          100000,  # Maximum Cost-Weighted Corridor Distance (17)
          40000]  # Maximum Euclidean Corridor Distance (18)
         )
